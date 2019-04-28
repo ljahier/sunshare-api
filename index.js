8c223dcb6fd5b98e34467c61729ff5b5c881b080
Lucas JAHIER <lucas@jahier.eu>
2019-04-28 10:39:35 +0200

    add routes and process with data

--- index.js
+++ index.js
@@ -15,13 +15,50 @@ app.get('/data', (req, res) => {
     client.connect((err, client) => {
         if (err) throw err
         let db = client.db(dbName)
-        db.collection('raw_data').find({}).toArray(function(err, result) {
+        db.collection('raw_data').find({}).toArray(function (err, result) {
             if (err) throw err;
             // console.log(result)
             res.json(result)
             client.close()
-          })
+        })
     })
+})
+
+app.get('/data/:clientID', (req, res) => {
+    let raw_client_id = req.params.clientID;
+    let raw_timestamp = req.query.t;
+    console.log(raw_timestamp);
+    let client = new MongoClient(mongodbUrl, { useNewUrlParser: true })
+    client.connect((err, client) => {
+        if (err) throw err
+        let db = client.db(dbName)
+        db.collection('raw_data').find({ client_id: raw_client_id }, { projection: { _id: 0 } }).toArray(function (err, result) {
+            if (err) throw err;
+            // console.log(result)
+            res.json(result)
+            client.close()
+        })
+    })
+
+})
+
+app.get('/data/:clientID/:requested', (req, res) => {
+    let raw_client_id = req.params.clientID;
+    let raw_requested = req.params.requested
+
+    let client = new MongoClient(mongodbUrl, { useNewUrlParser: true })
+    client.connect((err, client) => {
+        if (err) throw err
+        let db = client.db(dbName)
+
+        db.collection('raw_data').find({ client_id: raw_client_id }, { projection: { _id: 0, [raw_requested]: 1, timestamp: 1 }, limit: 100}).sort({'timestamp': -1}).toArray(function (err, result) {
+            if (err) throw err;
+            // console.log(result)
+            res.json(result)
+            client.close()
+        })
+    })
+
 })
 
 app.post('/data', (req, res) => {
@@ -42,3 +79,4 @@ app.post('/data', (req, res) => {
 })
 
 app.listen(port, () => console.log(`Server running on localhost:${port}`))
+
